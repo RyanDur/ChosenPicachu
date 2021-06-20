c38fe14bc455ebb0dada0b0f8117e657928713c7
Ryan Durling <ryan.p.durling@gmail.com>
2021-06-20 08:11:35 -0500

    make it a little fancier

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -4,31 +4,31 @@ import './About.css';
 export const About = () => {
     return <section>
         <ul className="accordion">
-            <li className="fold card">
+            <li className="fold">
                 <input id="first-checkbox" className="info-toggle off-screen" type="checkbox"/>
                 <label className="info-label" htmlFor="first-checkbox">{faker.lorem.word()}</label>
                 <p className="info">{faker.lorem.paragraphs(Math.floor(Math.random() * 6) + 1)}</p>
             </li>
 
-            <li className="fold card">
+            <li className="fold">
                 <input id="second-checkbox" className="info-toggle off-screen" type="checkbox"/>
                 <label className="info-label" htmlFor="second-checkbox">{faker.lorem.word()}</label>
                 <p className="info">{faker.lorem.paragraphs(Math.floor(Math.random() * 6) + 1)}</p>
             </li>
 
-            <li className="fold card">
+            <li className="fold">
                 <input id="third-checkbox" className="info-toggle off-screen" type="checkbox"/>
                 <label className="info-label" htmlFor="third-checkbox">{faker.lorem.word()}</label>
                 <p className="info">{faker.lorem.paragraphs(Math.floor(Math.random() * 6) + 1)}</p>
             </li>
 
-            <li className="fold card">
+            <li className="fold">
                 <input id="fourth-checkbox" className="info-toggle off-screen" type="checkbox"/>
                 <label className="info-label" htmlFor="fourth-checkbox">{faker.lorem.word()}</label>
                 <p className="info">{faker.lorem.paragraphs(Math.floor(Math.random() * 6) + 1)}</p>
             </li>
 
-            <li className="fold card">
+            <li className="fold">
                 <input id="fifth-checkbox" className="info-toggle off-screen" type="checkbox"/>
                 <label className="info-label" htmlFor="fifth-checkbox">{faker.lorem.word()}</label>
                 <p className="info">{faker.lorem.paragraphs(Math.floor(Math.random() * 6) + 1)}</p>
@@ -42,31 +42,31 @@ export const About = () => {
                 <input id="close-radio" className="info-toggle off-screen" type="radio" name="group"/>
                 <label className="info-label" htmlFor="close-radio">Close</label>
             </li>
-            <li className="fold card">
+            <li className="fold">
                 <input id="first-radio" className="info-toggle off-screen" type="radio" name="group"/>
                 <label className="info-label" htmlFor="first-radio">{faker.lorem.word()}</label>
                 <p className="info">{faker.lorem.paragraphs(Math.floor(Math.random() * 6) + 1)}</p>
             </li>
 
-            <li className="fold card">
+            <li className="fold">
                 <input id="second-radio" className="info-toggle off-screen" type="radio" name="group"/>
                 <label className="info-label" htmlFor="second-radio">{faker.lorem.word()}</label>
                 <p className="info">{faker.lorem.paragraphs(Math.floor(Math.random() * 6) + 1)}</p>
             </li>
 
-            <li className="fold card">
+            <li className="fold">
                 <input id="third-radio" className="info-toggle off-screen" type="radio" name="group"/>
                 <label className="info-label" htmlFor="third-radio">{faker.lorem.word()}</label>
                 <p className="info">{faker.lorem.paragraphs(Math.floor(Math.random() * 6) + 1)}</p>
             </li>
 
-            <li className="fold card">
+            <li className="fold">
                 <input id="fourth-radio" className="info-toggle off-screen" type="radio" name="group"/>
                 <label className="info-label" htmlFor="fourth-radio">{faker.lorem.word()}</label>
                 <p className="info">{faker.lorem.paragraphs(Math.floor(Math.random() * 6) + 1)}</p>
             </li>
 
-            <li className="fold card">
+            <li className="fold">
                 <input id="fifth-radio" className="info-toggle off-screen" type="radio" name="group"/>
                 <label className="info-label" htmlFor="fifth-radio">{faker.lorem.word()}</label>
                 <p className="info">{faker.lorem.paragraphs(Math.floor(Math.random() * 6) + 1)}</p>
