a100123a29da0561c1fc157b42b72317272e247a
Ryan Durling <ryan.p.durling@gmail.com>
2021-07-07 19:44:43 -0500

    refactor test

--- src/components/Users/__tests__/Users.spec.tsx
+++ src/components/Users/__tests__/Users.spec.tsx
@@ -16,16 +16,16 @@ describe('the users page', () => {
         firstRowFirstCell = within(table).getByTestId('cell-0-0');
         firstRowSecondCell = within(table).getByTestId('cell-1-0');
         firstRowThirdCell = within(table).getByTestId('cell-2-0');
+
+        fillOutUser(userInfo);
+        fillOutAddress(userInfo, 'home');
+        fillOutAddress(userInfo, 'work');
+        userEvent.type(screen.getByLabelText('Details'), userInfo.details!);
+        userEvent.click(screen.getByText('Add'));
     });
 
     describe('adding a user', () => {
         beforeEach(() => {
-            fillOutUser(userInfo);
-            fillOutAddress(userInfo, 'home');
-            fillOutAddress(userInfo, 'work');
-            userEvent.type(screen.getByLabelText('Details'), userInfo.details!);
-            userEvent.click(screen.getByText('Add'));
-
             fillOutUser(anotherUserInfo);
             fillOutAddress(anotherUserInfo, 'home');
             userEvent.click(screen.getByLabelText('Same as Home'));
@@ -51,66 +51,67 @@ describe('the users page', () => {
             expect(secondRowSecondCell).toHaveTextContent(userInfo.homeAddress.city);
             expect(secondRowThirdCell).toHaveTextContent('No');
         });
+    });
 
-        describe('viewing a user', () => {
-            beforeEach(() => {
-                table = screen.getByTestId('table');
-                firstRowThirdCell = within(table).getByTestId('cell-2-0');
+    describe('viewing a user', () => {
+        beforeEach(() => {
+            table = screen.getByTestId('table');
+            firstRowThirdCell = within(table).getByTestId('cell-2-0');
 
-                act(() => {
-                    userEvent.click(within(firstRowThirdCell).getByText('View'));
-                });
+            act(() => {
+                userEvent.click(within(firstRowThirdCell).getByText('View'));
             });
+        });
 
-            test('populating the form with the chosen user', async () => {
-                const form = screen.getByTestId('user-info-form');
-                expect(within(form).getByLabelText('First Name')).toHaveDisplayValue(anotherUserInfo.user.firstName);
-                expect(within(form).getByLabelText('Last Name')).toHaveDisplayValue(anotherUserInfo.user.lastName);
-                expect(within(form).getByLabelText('Email')).toHaveDisplayValue(anotherUserInfo.user.email!);
-            });
+        test('populating the form with the chosen user', async () => {
+            const form = screen.getByTestId('user-info-form');
+            expect(within(form).getByLabelText('First Name')).toHaveDisplayValue(userInfo.user.firstName);
+            expect(within(form).getByLabelText('Last Name')).toHaveDisplayValue(userInfo.user.lastName);
+            expect(within(form).getByLabelText('Email')).toHaveDisplayValue(userInfo.user.email!);
+        });
 
-            test('should not be able to change the uses data', async () => {
-                const form = screen.getByTestId('user-info-form');
-                expect(within(form).getByLabelText('First Name')).toHaveAttribute('readonly');
-                expect(within(form).getByLabelText('Last Name')).toHaveAttribute('readonly');
-                expect(within(form).getByLabelText('Email')).toHaveAttribute('readonly');
+        test('should not be able to change the uses data', async () => {
+            const form = screen.getByTestId('user-info-form');
+            expect(within(form).getByLabelText('First Name')).toHaveAttribute('readonly');
+            expect(within(form).getByLabelText('Last Name')).toHaveAttribute('readonly');
+            expect(within(form).getByLabelText('Email')).toHaveAttribute('readonly');
 
-                const avatarImage: HTMLImageElement = form.querySelector('#avatar')!;
-                const imageSrc = avatarImage.src;
-                userEvent.click(within(form).getByTestId('avatar-cell'));
-                expect(avatarImage.src).toEqual(imageSrc);
+            const avatarImage: HTMLImageElement = form.querySelector('#avatar')!;
+            const imageSrc = avatarImage.src;
+            userEvent.click(within(form).getByTestId('avatar-cell'));
+            expect(avatarImage.src).toEqual(imageSrc);
 
-                expect(within(form).getByTestId('home-street-address')).toHaveAttribute('readonly');
-                expect(within(form).getByTestId('home-street-address-2')).toHaveAttribute('readonly');
-                expect(within(form).getByTestId('home-city')).toHaveAttribute('readonly');
-                expect(within(form).getByTestId('home-state')).toHaveAttribute('readonly');
-                expect(within(form).getByTestId('home-zip')).toHaveAttribute('readonly');
+            expect(within(form).getByTestId('home-street-address')).toHaveAttribute('readonly');
+            expect(within(form).getByTestId('home-street-address-2')).toHaveAttribute('readonly');
+            expect(within(form).getByTestId('home-city')).toHaveAttribute('readonly');
+            expect(within(form).getByTestId('home-state')).toHaveAttribute('readonly');
+            expect(within(form).getByTestId('home-zip')).toHaveAttribute('readonly');
 
-                expect(within(form).getByTestId('work-street-address')).toHaveAttribute('readonly');
-                expect(within(form).getByTestId('work-street-address-2')).toHaveAttribute('readonly');
-                expect(within(form).getByTestId('work-city')).toHaveAttribute('readonly');
-                expect(within(form).getByTestId('work-state')).toHaveAttribute('readonly');
-                expect(within(form).getByTestId('work-zip')).toHaveAttribute('readonly');
+            expect(within(form).getByTestId('work-street-address')).toHaveAttribute('readonly');
+            expect(within(form).getByTestId('work-street-address-2')).toHaveAttribute('readonly');
+            expect(within(form).getByTestId('work-city')).toHaveAttribute('readonly');
+            expect(within(form).getByTestId('work-state')).toHaveAttribute('readonly');
+            expect(within(form).getByTestId('work-zip')).toHaveAttribute('readonly');
 
-                expect(within(form).getByLabelText('Details')).toHaveAttribute('readonly');
-            });
+            expect(within(form).getByLabelText('Details')).toHaveAttribute('readonly');
+        });
 
-            test('adding a new user', () => {
-                userEvent.click(screen.getByText('Add New User'));
-                fillOutUser(anotherUserInfo);
-                fillOutAddress(anotherUserInfo, 'home');
-                userEvent.click(screen.getByLabelText('Same as Home'));
+        test('adding a new user', () => {
+            userEvent.click(screen.getByText('Add New User'));
+            fillOutUser(anotherUserInfo);
+            fillOutAddress(anotherUserInfo, 'home');
+            userEvent.click(screen.getByLabelText('Same as Home'));
+            userEvent.click(screen.getByText('Add'));
 
-                expect(firstRowFirstCell).toHaveTextContent(`${anotherUserInfo.user.firstName} ${anotherUserInfo.user.lastName}`);
-                expect(firstRowSecondCell).toHaveTextContent(anotherUserInfo.homeAddress.city);
-                expect(firstRowThirdCell).toHaveTextContent('Yes');
-            });
+            expect(firstRowFirstCell).toHaveTextContent(`${anotherUserInfo.user.firstName} ${anotherUserInfo.user.lastName}`);
+            expect(firstRowSecondCell).toHaveTextContent(anotherUserInfo.homeAddress.city);
+            expect(firstRowThirdCell).toHaveTextContent('Yes');
+        });
 
-            test('should remove button if able to add a new user', () => {
-                userEvent.click(screen.getByText('Add New User'));
+        test('should remove button if able to add a new user', () => {
+            userEvent.click(screen.getByText('Add New User'));
 
-                expect(screen.queryByText('Add New User')).not.toBeInTheDocument();
-            });
+            expect(screen.queryByText('Add New User')).not.toBeInTheDocument();
         });
     });
 });
