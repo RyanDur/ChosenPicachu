5a599f14ebd44b7ce303bc931a3a6b2d99300cbe
Ryan Durling <ryan.p.durling@gmail.com>
2021-07-18 11:54:35 -0500

    first pass at a loading state for pieces

--- src/components/ArtGallery/index.tsx
+++ src/components/ArtGallery/index.tsx
@@ -1,6 +1,7 @@
 import {FC, useEffect, useState} from 'react';
 import {Art} from '../../data/types';
 import {data} from '../../data';
+import './ArtGallery.layout.scss';
 
 export const ArtGallery: FC = () => {
     const [art, updateArtWork] = useState<Art>();
@@ -9,11 +10,10 @@ export const ArtGallery: FC = () => {
         data.getAllArt(updateArtWork);
     }, []);
 
-    return <article>{art?.pieces
-        .map(piece => <article key={piece.imageId} data-testid="art">
-            <img className="card"
-                src={`https://www.artic.edu/iiif/2/${piece.imageId}/full/200,/0/default.jpg`}
-                 alt={piece.title} loading="lazy" data-testid="piece"/>
-        </article>)
-    }</article>;
+    return <section id="art-gallery">{art?.pieces
+        .map(piece => <img className="card loading" key={piece.imageId}
+                           onLoad={event => event.currentTarget.classList.remove('loading')}
+                           alt={piece.title} loading="lazy" data-testid="piece" tabIndex={0}
+                           src={`https://www.artic.edu/iiif/2/${piece.imageId}/full/200,/0/default.jpg`}/>)
+    }</section>;
 };
