781a575723632328839185a5e06518a4f25cab77
Ryan Durling <ryan.p.durling@gmail.com>
2021-08-02 13:32:05 -0500

    Header to title component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,5 @@ export {Home} from './Home';
 export {About} from './About';
 export {Users} from './Users';
 export {ArtGallery} from './ArtGallery';
-export {Header} from './Header';
+export {Title} from './Header';
 export {ArtPiece, ArtPieceContext, useArtPieceContext} from './ArtGallery/ArtPiece';
