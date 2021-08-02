afc455df861d847378dd659b332dc630bbeefd9f
Ryan Durling <ryan.p.durling@gmail.com>
2021-08-02 13:00:30 -0500

    refactor header to use the context outside the returned component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,15 +5,18 @@ import {useArtPiece} from '../ArtGallery/ArtPiece/Context';
 import './Header.scss';
 import './Header.layout.scss';
 
-export const Header = () =>
-    <header id="app-header">
+export const Header = () => {
+    const {piece} = useArtPiece();
+
+    return <header id="app-header">
         <h1 className="title">
             <Switch>
                 <Route path={Paths.home} exact>Home</Route>
                 <Route path={Paths.about} exact>About</Route>
                 <Route path={Paths.users} exact>Users</Route>
                 <Route path={Paths.artGallery} exact>Art Gallery</Route>
-                <Route path={Paths.artGalleryPiece} exact>{useArtPiece().piece.title}</Route>
+                <Route path={Paths.artGalleryPiece} exact>{piece.title}</Route>
             </Switch>
         </h1>
     </header>;
+};
