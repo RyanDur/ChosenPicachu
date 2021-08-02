b4fdc78ef13d188e0b1934c6510a875506f226ba
Ryan Durling <ryan.p.durling@gmail.com>
2021-08-02 15:25:18 -0500

    title component back into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {Link, Route, Switch} from 'react-router-dom';
-import {About, ArtGallery, ArtPiece, ArtPieceContext, Title, Home, useArtPieceContext, Users} from './components';
+import {About, ArtGallery, ArtPiece, Home, Users} from './components';
+import {useArtPiece} from './components/ArtGallery/ArtPiece';
 import './App.scss';
-import './App.layout.css';
+import './App.layout.scss';
 
 export enum Paths {
     home = '/',
@@ -13,37 +14,49 @@ export enum Paths {
     repo = 'https://github.com/RyanDur/ChosenPicachu',
 }
 
-export const App = () => <ArtPieceContext.Provider value={useArtPieceContext()}>
-    <header id="app-header">
-        <Title/>
-    </header>
-    <aside id="side-nav">
-        <nav id="app-navigation" className="inset-right">
-            <Link id="navigate-home" className="path" to={Paths.home}>Home</Link>
-            <Link id="navigate-about" className="path" to={Paths.about}>About</Link>
-            <Link id="navigate-users" className="path" to={Paths.users}>Users</Link>
-            <Link id="navigate-form" className="path" to={Paths.artGallery}>Art</Link>
-            <a id="navigate-repo" className="path" href={Paths.repo}
-               rel="noopener noreferrer" target="_blank">Repo</a>
-        </nav>
-    </aside>
-    <main>
-        <Switch>
-            <Route path={Paths.home} exact>
-                <Home/>
-            </Route>
-            <Route path={Paths.about} exact>
-                <About/>
-            </Route>
-            <Route path={Paths.users} exact>
-                <Users/>
-            </Route>
-            <Route path={Paths.artGallery} exact>
-                <ArtGallery/>
-            </Route>
-            <Route path={Paths.artGalleryPiece} exact>
-                <ArtPiece/>
-            </Route>
-        </Switch>
-    </main>
-</ArtPieceContext.Provider>;
+export const App = () => {
+    const {piece} = useArtPiece();
+
+    return <>
+        <header id="app-header" data-testid="header">
+            <h1 className="title">
+                <Switch>
+                    <Route path={Paths.home} exact>Home</Route>
+                    <Route path={Paths.about} exact>About</Route>
+                    <Route path={Paths.users} exact>Users</Route>
+                    <Route path={Paths.artGallery} exact>Art Gallery</Route>
+                    <Route path={Paths.artGalleryPiece} exact>{piece.title}</Route>
+                </Switch>
+            </h1>
+        </header>
+        <aside id="side-nav" data-testid="aside">
+            <nav id="app-navigation" className="inset-right">
+                <Link id="navigate-home" className="path" to={Paths.home}>Home</Link>
+                <Link id="navigate-about" className="path" to={Paths.about}>About</Link>
+                <Link id="navigate-users" className="path" to={Paths.users}>Users</Link>
+                <Link id="navigate-form" className="path" to={Paths.artGallery}>Art</Link>
+                <a id="navigate-repo" className="path" href={Paths.repo}
+                   rel="noopener noreferrer" target="_blank">Repo</a>
+            </nav>
+        </aside>
+        <main data-testid="main">
+            <Switch>
+                <Route path={Paths.home} exact>
+                    <Home/>
+                </Route>
+                <Route path={Paths.about} exact>
+                    <About/>
+                </Route>
+                <Route path={Paths.users} exact>
+                    <Users/>
+                </Route>
+                <Route path={Paths.artGallery} exact>
+                    <ArtGallery/>
+                </Route>
+                <Route path={Paths.artGalleryPiece} exact>
+                    <ArtPiece/>
+                </Route>
+            </Switch>
+        </main>
+    </>;
+};
