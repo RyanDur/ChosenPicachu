35dd396c3e7d342d124d66a774ab0d01665c08fe
Ryan Durling <ryan.p.durling@gmail.com>
2021-08-18 13:27:11 -0500

    create env file; refactor data component

--- src/util/URL/index.ts
+++ src/util/URL/index.ts
@@ -1,9 +1,16 @@
-export const toQueryString = (queryObj = {}): string => '?' + Object.entries(queryObj)
-    .filter(([, value]) => !!value)
-    .map(([key, value]) => `${key}=${value}`).join('&');
+import {domain} from '../../config';
 
-export const toQueryObj = (queryString: string, defaultObj = {}) => queryString ? queryString.replace('?', '')
-    .split('&')
-    .map(query => query.split('='))
-    .map(([key, value]) => ({[key]: value}))
-    .reduce((acc, param) => ({...acc, ...param}), defaultObj) : defaultObj;
+export const toQueryString = (queryObj?: Record<string, unknown>): string =>
+    queryObj ? `?${Object.entries(queryObj)
+        .filter(([, value]) => !!value)
+        .map(([key, value]) => `${key}=${value}`).join('&')}` : '';
+
+export const toQueryObj = (queryString: string, defaultObj = {}) =>
+    queryString ? queryString.replace('?', '')
+        .split('&')
+        .map(query => query.split('='))
+        .map(([key, value]) => ({[key]: value}))
+        .reduce((acc, param) => ({...acc, ...param}), defaultObj) : defaultObj;
+
+export const toUrl = (path: string, queryParams?: Record<string, unknown>) =>
+    `${domain}${path}${toQueryString(queryParams)}`;
