9db74226d863d7d4590949798ef1c0d3ec454a47
Ryan Durling <ryan.p.durling@gmail.com>
2021-08-26 18:32:28 -0500

    keep params

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -17,11 +17,11 @@ interface Props {
 
 export const Tabs: FC<Props> = ({values, id}) => {
     const {pathname} = useLocation();
-    const {tab} = useQuery<{ tab: string }>();
+    const {tab, ...rest} = useQuery<{ tab: string }>();
 
     return <nav id={id} className="tabs">{values.map(({param, display}) =>
         <h3 className={join('tab', tab === param && 'current')} key={param}>
-            <Link to={`${pathname}${toQueryString({tab: param})}`}
+            <Link to={`${pathname}${toQueryString({tab: param, ...rest, page: 1})}`}
                   className="path">{display}</Link>
         </h3>
     )}</nav>;
