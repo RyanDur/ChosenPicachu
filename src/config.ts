9377b9ba6117b434452c22e36a4e1745814e30c3
Ryan Durling <ryan.p.durling@gmail.com>
2021-08-26 18:35:35 -0500

    default limits

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
 export const aicDomain = process.env.REACT_APP_API_AIC;
 export const harvardDomain = process.env.REACT_APP_HARVARD_API;
 export const harvardAPIKey = process.env.REACT_APP_HARVARD_API_KEY;
+export const defaultRecordLimit = 12;
+export const defaultAutocompleteLimit = 5;
