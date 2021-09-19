6bc3a50cae0954e0b9df8e5ef0017c1d3ef44a9f
Ryan Durling <ryan.p.durling@gmail.com>
2021-09-19 08:38:39 -0500

    lazy evaluate

--- src/data/artGallery/index.ts
+++ src/data/artGallery/index.ts
@@ -13,27 +13,27 @@ export const artGallery = {
     getAllArt: ({page, size, source, search}: GetAllArt): OnAsyncEvent<AllArt, HTTPError> =>
         asyncEvent(URI.from({source, params: {page, search, limit: size}})
             .map(uri => http.get(uri).flatMap(response => ({
-                [Source.AIC]: aic.validateAllArt(response).map(aic.toAllArt),
-                [Source.HARVARD]: harvard.validateAllArt(response).map(harvard.toAllArt),
-                [Source.RIJKS]: rijks.validateAllArt(response).map(rijks.toAllArt(page)),
-                [Source.UNKNOWN]: unknownSource<AllArt>()
-            })[toSource(source)])).orElse(unknownSource<AllArt>())),
+                [Source.AIC]: () => aic.validateAllArt(response).map(aic.toAllArt),
+                [Source.HARVARD]: () => harvard.validateAllArt(response).map(harvard.toAllArt),
+                [Source.RIJKS]: () => rijks.validateAllArt(response).map(rijks.toAllArt(page)),
+                [Source.UNKNOWN]: () => unknownSource<AllArt>()
+            })[toSource(source)]())).orElse(unknownSource<AllArt>())),
 
     getArt: ({id, source}: GetArt): OnAsyncEvent<Art, HTTPError> =>
         asyncEvent(URI.from({source: source, path: [id]})
             .map(uri => http.get(uri).flatMap(response => ({
-                [Source.AIC]: aic.validateArt(response).map(aic.toArt),
-                [Source.HARVARD]: harvard.validateArt(response).map(harvard.toArt),
-                [Source.RIJKS]: rijks.validateArt(response).map(rijks.toArt),
-                [Source.UNKNOWN]: unknownSource<Art>()
-            })[toSource(source)])).orElse(unknownSource<Art>())),
+                [Source.AIC]: () => aic.validateArt(response).map(aic.toArt),
+                [Source.HARVARD]: () => harvard.validateArt(response).map(harvard.toArt),
+                [Source.RIJKS]: () => rijks.validateArt(response).map(rijks.toArt),
+                [Source.UNKNOWN]: () => unknownSource<Art>()
+            })[toSource(source)]())).orElse(unknownSource<Art>())),
 
     searchForArt: ({search, source}: SearchArt): OnAsyncEvent<SearchOptions, HTTPError> =>
         asyncEvent(URI.createSearchFrom(search, source)
             .map(uri => http.get(uri).flatMap(response => ({
-                [Source.AIC]: aic.validateSearch(response).map(aic.toSearch),
-                [Source.HARVARD]: harvard.validateSearch(response).map(harvard.toSearch),
-                [Source.RIJKS]: rijks.validateSearch(response).map(rijks.toSearch),
-                [Source.UNKNOWN]: unknownSource<SearchOptions>()
-            })[toSource(source)])).orElse(unknownSource<SearchOptions>()))
+                [Source.AIC]: () => aic.validateSearch(response).map(aic.toSearch),
+                [Source.HARVARD]: () => harvard.validateSearch(response).map(harvard.toSearch),
+                [Source.RIJKS]: () => rijks.validateSearch(response).map(rijks.toSearch),
+                [Source.UNKNOWN]: () => unknownSource<SearchOptions>()
+            })[toSource(source)]())).orElse(unknownSource<SearchOptions>()))
 };
