4b9d2b48d7136f8a2408a820ee951795e2b6e504
Ryan Durling <ryan.p.durling@gmail.com>
2021-09-21 20:35:16 -0500

    refactor

--- src/data/artGallery/index.ts
+++ src/data/artGallery/index.ts
@@ -11,7 +11,7 @@ import {
     SearchOptionsRequests,
     Source
 } from './types';
-import {asyncEvent, asyncResult, MatchOn, maybe, OnAsyncEvent} from '@ryandur/sand';
+import {asyncEvent, asyncResult, maybe, OnAsyncEvent} from '@ryandur/sand';
 import {explanation, Explanation, HTTPError} from '../types';
 import {aic} from './aic';
 import {harvard} from './harvard';
@@ -22,35 +22,32 @@ const unknownSource = <T>() => asyncResult.failure<T, Explanation<HTTPError>>(ex
 
 export const artGallery = {
     getAllArt: ({page, size, source, search}: GetAllArt): OnAsyncEvent<AllArt, Explanation<HTTPError>> =>
-        asyncEvent(maybe.of(matchSource<AllArtRequests | undefined>(source, {
+        asyncEvent(maybe.of(matchSource<AllArtRequests>(source, {
             [Source.AIC]: () => aic.allArt,
             [Source.HARVARD]: () => harvard.allArt,
             [Source.RIJKS]: () => rijks.allArt(page),
-            [MatchOn.DEFAULT]: () => undefined
         })).map(resource => http.get(resource.endpoint({params: {page, size, search}}))
             .flatMap(resource.validate)
             .map(resource.toAllArt))
-            .orElse(unknownSource<AllArt>())),
+            .orElse(unknownSource())),
 
     getArt: ({id, source}: GetArt): OnAsyncEvent<Art, Explanation<HTTPError>> =>
-        asyncEvent(maybe.of(matchSource<ArtRequests | undefined>(source, {
+        asyncEvent(maybe.of(matchSource<ArtRequests>(source, {
             [Source.AIC]: () => aic.art,
             [Source.HARVARD]: () => harvard.art,
             [Source.RIJKS]: () => rijks.art,
-            [MatchOn.DEFAULT]: () => undefined
         })).map(resource => http.get(resource.endpoint({path: [id]}))
             .flatMap(resource.validate)
             .map(resource.toArt))
-            .orElse(unknownSource<Art>())),
+            .orElse(unknownSource())),
 
     searchForArt: ({search, source}: SearchArt): OnAsyncEvent<SearchOptions, Explanation<HTTPError>> =>
-        asyncEvent(maybe.of(matchSource<SearchOptionsRequests | undefined>(source, {
+        asyncEvent(maybe.of(matchSource<SearchOptionsRequests>(source, {
             [Source.AIC]: () => aic.searchOptions,
             [Source.HARVARD]: () => harvard.searchOptions,
             [Source.RIJKS]: () => rijks.searchOptions,
-            [MatchOn.DEFAULT]: () => undefined
         })).map(resource => http.get(resource.endpoint(search))
             .flatMap(resource.validate)
             .map(resource.toSearchOptions))
-            .orElse(unknownSource<SearchOptions>()))
+            .orElse(unknownSource()))
 };
