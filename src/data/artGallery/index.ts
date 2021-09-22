7586c35fa0fa3e35866dd87602e98923848cd75c
Ryan Durling <ryan.p.durling@gmail.com>
2021-09-21 20:42:11 -0500

    refactor

--- src/data/artGallery/index.ts
+++ src/data/artGallery/index.ts
@@ -26,28 +26,25 @@ export const artGallery = {
             [Source.AIC]: () => aic.allArt,
             [Source.HARVARD]: () => harvard.allArt,
             [Source.RIJKS]: () => rijks.allArt(page),
-        })).map(resource => http.get(resource.endpoint({params: {page, size, search}}))
-            .flatMap(resource.validate)
-            .map(resource.toAllArt))
-            .orElse(unknownSource())),
+        })).map(({endpoint, validate, toAllArt}) =>
+            http.get(endpoint({params: {page, size, search}})).flatMap(validate).map(toAllArt)
+        ).orElse(unknownSource())),
 
     getArt: ({id, source}: GetArt): OnAsyncEvent<Art, Explanation<HTTPError>> =>
         asyncEvent(maybe.of(matchSource<ArtRequests>(source, {
             [Source.AIC]: () => aic.art,
             [Source.HARVARD]: () => harvard.art,
             [Source.RIJKS]: () => rijks.art,
-        })).map(resource => http.get(resource.endpoint({path: [id]}))
-            .flatMap(resource.validate)
-            .map(resource.toArt))
-            .orElse(unknownSource())),
+        })).map(({endpoint, validate, toArt}) =>
+            http.get(endpoint({path: [id]})).flatMap(validate).map(toArt)
+        ).orElse(unknownSource())),
 
     searchForArt: ({search, source}: SearchArt): OnAsyncEvent<SearchOptions, Explanation<HTTPError>> =>
         asyncEvent(maybe.of(matchSource<SearchOptionsRequests>(source, {
             [Source.AIC]: () => aic.searchOptions,
             [Source.HARVARD]: () => harvard.searchOptions,
             [Source.RIJKS]: () => rijks.searchOptions,
-        })).map(resource => http.get(resource.endpoint(search))
-            .flatMap(resource.validate)
-            .map(resource.toSearchOptions))
-            .orElse(unknownSource()))
+        })).map(({endpoint, validate, toSearchOptions}) =>
+            http.get(endpoint(search)).flatMap(validate).map(toSearchOptions)
+        ).orElse(unknownSource()))
 };
