92e3104a949ff901cd69950e80276cf9bc8453db
Ryan Durling <ryan.p.durling@gmail.com>
2024-07-30 19:51:05 -0500

    name env variables correctly

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
-export const aicDomain = import.meta.env.REACT_APP_API_AIC;
-export const harvardDomain = import.meta.env.REACT_APP_HARVARD_API;
-export const harvardAPIKey = import.meta.env.REACT_APP_HARVARD_API_KEY;
-export const rijksDomain = import.meta.env.REACT_APP_RIJKS_API;
-export const rijksAPIKey = import.meta.env.REACT_APP_RIJKS_API_KEY;
+export const aicDomain = import.meta.env.VITE_APP_API_AIC;
+export const harvardDomain = import.meta.env.VITE_APP_HARVARD_API;
+export const harvardAPIKey = import.meta.env.VITE_APP_HARVARD_API_KEY;
+export const rijksDomain = import.meta.env.VITE_APP_RIJKS_API;
+export const rijksAPIKey = import.meta.env.VITE_APP_RIJKS_API_KEY;
 export const defaultRecordLimit = 8;
 export const defaultSearchLimit = 5;
