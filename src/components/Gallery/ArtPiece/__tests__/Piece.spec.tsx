5449557767ea8bfc8f20168cba96c0d8c6d41780
Ryan Durling <ryan.p.durling@gmail.com>
2024-08-04 21:10:45 -0500

    remove artPiece. can get all the information from the response

--- src/components/Gallery/ArtPiece/__tests__/Piece.spec.tsx
+++ src/components/Gallery/ArtPiece/__tests__/Piece.spec.tsx
@@ -6,16 +6,8 @@ import {faker} from '@faker-js/faker';
 import {Paths} from '../../../../routes/Paths.ts';
 import {Source} from '../../../../data/artGallery/types/resource.ts';
 import {AICArtResponse} from '../../../../data/artGallery/aic/types.ts';
-import {Art} from '../../../../data/artGallery/types/response.ts';
 
 describe('viewing a piece', () => {
-  const artPiece: Art = {
-    id: faker.lorem.word(),
-    image: faker.internet.url(),
-    title: faker.lorem.words(),
-    altText: faker.lorem.sentence(),
-    artistInfo: faker.lorem.paragraph()
-  };
   const aicArtResponse: AICArtResponse = {
     data: {
       id: faker.number.int(),
@@ -41,9 +33,8 @@ describe('viewing a piece', () => {
 
   describe('when the art piece is loaded', () => {
     const params = {
-      initialRoute: `${Paths.artGallery}/${artPiece.id}`,
+      initialRoute: `${Paths.artGallery}/${aicArtResponse.data.id}`,
       path: Paths.artGalleryPiece,
-      pieceState: artPiece,
       params: {tab: Source.AIC}
     };
 
@@ -54,7 +45,7 @@ describe('viewing a piece', () => {
     it("should display the artist's info", async () => {
       renderWithRouter(<ArtPiece/>, params);
 
-      expect(await screen.findByText(artPiece.artistInfo)).toBeInTheDocument();
+      expect(await screen.findByText(aicArtResponse.data.artist_display)).toBeInTheDocument();
     });
 
     it('should not have the error image', async () => {
@@ -65,7 +56,7 @@ describe('viewing a piece', () => {
   });
 
   test('when getting the piece has errored', async () => {
-    fetchMock.mockResponse(HTTPError.SERVER_ERROR, {status: 400});
+    fetchMock.mockResponse(HTTPError.SERVER_ERROR, {status: 500});
     renderWithRouter(<ArtPiece/>, {
       initialRoute: `${Paths.artGallery}/1234`,
       path: `${Paths.artGalleryPiece}`
