2322169c6eb82ed7c99ecd9da322bb98235be598
Ryan Durling <ryan.p.durling@gmail.com>
2024-08-05 17:56:36 -0500

    simplify Image test

--- src/components/Gallery/Image/__tests__/Image.spec.tsx
+++ src/components/Gallery/Image/__tests__/Image.spec.tsx
@@ -17,138 +17,63 @@ describe('the image', () => {
   };
   const image = `piece-${piece.id}`;
 
-  beforeEach(() => {
-    window.scrollTo = vi.fn();
+  beforeEach(() => window.scrollTo = vi.fn());
+
+  test('on loading', () => {
+    renderWithRouter(<Image piece={piece}/>, {params: {page: 3, tab: 'aic'}});
+
+    expect(screen.queryByTestId('loading')).toBeInTheDocument();
+    expect(screen.queryByTestId('error')).not.toBeInTheDocument();
   });
 
-  describe('with link enabled', () => {
-    it('should be loading', () => {
-      renderWithRouter(<Image piece={piece}/>, {params: {page: 3, tab: 'aic'}});
+  test('when image loaded', () => {
+    renderWithRouter(<Image piece={piece}/>, {params: {page: 3, tab: 'aic'}});
 
-      expect(screen.queryByTestId('loading')).toBeInTheDocument();
-    });
+    fireEvent.load(screen.getByTestId(image));
 
-    it('should not be errored', () => {
-      renderWithRouter(<Image piece={piece}/>, {params: {page: 3, tab: 'aic'}});
-
-      expect(screen.queryByTestId('error')).not.toBeInTheDocument();
-    });
-
-    describe('on image load', () => {
-      it('should not be loading', () => {
-        renderWithRouter(<Image piece={piece}/>, {params: {page: 3, tab: 'aic'}});
-
-        fireEvent.load(screen.getByTestId(image));
-
-        expect(screen.queryByTestId('loading')).not.toBeInTheDocument();
-      });
-
-      it('should contain the image', () => {
-        renderWithRouter(<Image piece={piece}/>, {params: {page: 3, tab: 'aic'}});
-
-        fireEvent.load(screen.getByTestId(image));
-
-        expect(screen.queryByTestId(image)).toBeInTheDocument();
-      });
-
-      it('should not be errored', () => {
-        renderWithRouter(<Image piece={piece}/>, {params: {page: 3, tab: 'aic'}});
-
-        fireEvent.load(screen.getByTestId(image));
-
-        expect(screen.queryByTestId('error')).not.toBeInTheDocument();
-      });
-
-      it('should change location when clicked', async () => {
-        const rendered = renderWithRouter(<Image piece={piece}/>, {params: {page: 3, tab: 'aic'}});
-
-        fireEvent.load(screen.getByTestId(image));
-        await userEvent.click(screen.getByTestId(image));
-
-        expect(rendered().testLocation?.pathname).toEqual(`${Paths.artGallery}/${piece.id}`);
-      });
-
-      it('should scroll to the top of the page', async () => {
-        renderWithRouter(<Image piece={piece}/>, {params: {page: 3, tab: 'aic'}});
-
-        fireEvent.load(screen.getByTestId(image));
-        await userEvent.click(screen.getByTestId(image));
-
-        expect(window.scrollTo).toHaveBeenCalledWith(0, 0);
-      });
-    });
-
-    describe('on image load error', () => {
-
-      it('should not contain the piece', () => {
-        renderWithRouter(<Image piece={piece}/>, {params: {page: 3, tab: 'aic'}});
-
-        fireEvent.error(screen.getByTestId(image));
-
-        expect(screen.queryByTestId(image)).not.toBeInTheDocument();
-      });
-
-      it('should be errored', () => {
-        renderWithRouter(<Image piece={piece}/>, {params: {page: 3, tab: 'aic'}});
-
-        fireEvent.error(screen.getByTestId(image));
-
-        expect(screen.queryByTestId('error')).toBeInTheDocument();
-      });
-
-      it('should not be loading', () => {
-        renderWithRouter(<Image piece={piece}/>, {params: {page: 3, tab: 'aic'}});
-
-        fireEvent.error(screen.getByTestId(image));
-
-        expect(screen.queryByTestId('loading')).not.toBeInTheDocument();
-      });
-    });
+    expect(screen.queryByTestId('loading')).not.toBeInTheDocument();
+    expect(screen.queryByTestId(image)).toBeInTheDocument();
+    expect(screen.queryByTestId('error')).not.toBeInTheDocument();
   });
 
-  describe('without an image', () => {
-    it('should be loading', () => {
-      renderWithRouter(<Image piece={{...piece, image: undefined}}/>, {
-        params: {page: 3, tab: Source.AIC}
-      });
+  test('when choosing an image', async () => {
+    const rendered = renderWithRouter(<Image piece={piece}/>, {params: {page: 3, tab: 'aic'}});
 
-      expect(screen.queryByTestId('loading')).not.toBeInTheDocument();
-    });
+    fireEvent.load(screen.getByTestId(image));
+    await userEvent.click(screen.getByTestId(image));
 
-    it('should contain the image', () => {
-      renderWithRouter(<Image piece={{...piece, image: undefined}}/>, {
-        params: {page: 3, tab: Source.AIC}
-      });
-
-      expect(screen.queryByTestId(image)).not.toBeInTheDocument();
-    });
-
-    it('should not be errored', () => {
-      renderWithRouter(<Image piece={{...piece, image: undefined}}/>, {
-        params: {page: 3, tab: Source.AIC}
-      });
-
-      expect(screen.queryByTestId('error')).toBeInTheDocument();
-    });
+    expect(rendered().testLocation?.pathname).toEqual(`${Paths.artGallery}/${piece.id}`);
+    expect(window.scrollTo).toHaveBeenCalledWith(0, 0);
   });
 
-  describe('disabling the link', () => {
-    it('should not change location when clicked', async () => {
-      const rendered = renderWithRouter(<Image piece={piece} linkEnabled={false}/>,
-        {path: Paths.artGallery, initialRoute: Paths.artGallery, params: {page: 3, tab: Source.AIC}});
+  test('on image load error', () => {
+    renderWithRouter(<Image piece={piece}/>, {params: {page: 3, tab: 'aic'}});
 
-      await userEvent.click(await screen.findByTestId(image));
+    fireEvent.error(screen.getByTestId(image));
 
-      expect(rendered().testLocation?.pathname).toEqual(Paths.artGallery);
+    expect(screen.queryByTestId(image)).not.toBeInTheDocument();
+    expect(screen.queryByTestId('error')).toBeInTheDocument();
+    expect(screen.queryByTestId('loading')).not.toBeInTheDocument();
+  });
+
+  test('without an image', () => {
+    renderWithRouter(<Image piece={{...piece, image: undefined}}/>, {
+      params: {page: 3, tab: Source.AIC}
     });
 
-    it('should not scroll to the top of the page', async () => {
-      renderWithRouter(<Image piece={piece} linkEnabled={false}/>,
-        {path: Paths.artGallery, initialRoute: Paths.artGallery});
+    expect(screen.queryByTestId('error')).toBeInTheDocument();
+    expect(screen.queryByTestId('loading')).not.toBeInTheDocument();
+    expect(screen.queryByTestId(image)).not.toBeInTheDocument();
+  });
 
-      await userEvent.click(screen.getByTestId(image));
+  test('when the image is disabled', async () => {
+    const rendered = renderWithRouter(<Image piece={piece} linkEnabled={false}/>,
+      {path: Paths.artGallery, initialRoute: Paths.artGallery, params: {page: 3, tab: Source.AIC}});
 
-      expect(window.scrollTo).not.toHaveBeenCalled();
-    });
+    fireEvent.load(screen.getByTestId(image));
+    await userEvent.click(await screen.findByTestId(image));
+
+    expect(rendered().testLocation?.pathname).toEqual(Paths.artGallery);
+    expect(window.scrollTo).not.toHaveBeenCalled();
   });
 });
