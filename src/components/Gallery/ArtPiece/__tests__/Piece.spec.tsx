28392566145c42210cefb754a8d2e3208857b8f9
Ryan Durling <ryan.p.durling@gmail.com>
2024-08-04 21:00:05 -0500

    remove mocking from Piece.spec.tsx

--- src/components/Gallery/ArtPiece/__tests__/Piece.spec.tsx
+++ src/components/Gallery/ArtPiece/__tests__/Piece.spec.tsx
@@ -1,24 +1,14 @@
 import {screen, waitFor} from '@testing-library/react';
-import {ArtPiece, useArtPiece} from '../index';
-import {data} from '../../../../data';
+import {ArtPiece} from '../index';
 import {renderWithRouter} from '../../../../__tests__/util';
-import {Art} from '../../../../data/artGallery/types/response';
-import {Explanation, explanation, HTTPError} from '../../../../data/types';
-import {asyncFailure, asyncSuccess} from '@ryandur/sand';
+import {HTTPError} from '../../../../data/types';
 import {faker} from '@faker-js/faker';
 import {Paths} from '../../../../routes/Paths.ts';
-import {Mock} from 'vitest';
 import {Source} from '../../../../data/artGallery/types/resource.ts';
-
-vi.mock('../index', async () => ({
-  ...(await vi.importActual('../index')),
-  useArtPiece: vi.fn()
-}));
-const success = asyncSuccess;
-const failure = asyncFailure;
+import {AICArtResponse} from '../../../../data/artGallery/aic/types.ts';
+import {Art} from '../../../../data/artGallery/types/response.ts';
 
 describe('viewing a piece', () => {
-  const mockUsePieceGallery = useArtPiece as Mock;
   const mockPiece: Art = {
     id: faker.lorem.word(),
     image: faker.internet.url(),
@@ -26,29 +16,25 @@ describe('viewing a piece', () => {
     altText: faker.lorem.sentence(),
     artistInfo: faker.lorem.paragraph()
   };
-
-  beforeEach(() => mockUsePieceGallery.mockReturnValue({
-    piece: mockPiece,
-    updatePiece: vi.fn(),
-    reset: vi.fn()
-  }));
+  const aicPieceResponse: AICArtResponse = {
+    data: {
+      id: faker.number.int(),
+      title: faker.lorem.words(),
+      term_titles: [faker.lorem.sentence()],
+      artist_display: faker.lorem.paragraph()
+    }
+  };
 
   describe('loading the piece of art', () => {
     it('should be loading', async () => {
-      data.artGallery.getArt = () => ({
-        ...success(mockPiece),
-        onPending: dispatch => {
-          dispatch(true);
-          return success(mockPiece);
-        }
-      });
+      fetchMock.mockResponse(JSON.stringify(aicPieceResponse));
 
       renderWithRouter(<ArtPiece/>, {
         initialRoute: `${Paths.artGallery}/1234`,
         path: `${Paths.artGalleryPiece}`
       });
 
-      expect(await screen.findByTestId('loading-piece')).toBeInTheDocument();
+      await waitFor(() => expect(screen.getByTestId('loading-piece')).toBeInTheDocument());
     });
   });
 
@@ -56,24 +42,18 @@ describe('viewing a piece', () => {
     const params = {
       initialRoute: `${Paths.artGallery}/${mockPiece.id}`,
       path: Paths.artGalleryPiece,
+      pieceState: mockPiece,
       params: {tab: Source.AIC}
     };
 
     beforeEach(async () => {
-      data.artGallery.getArt =
-        vi.fn(() => success<Art, Explanation<HTTPError>>(mockPiece));
+      fetchMock.mockResponse(JSON.stringify(aicPieceResponse));
     });
 
     it("should display the artist's info", async () => {
       renderWithRouter(<ArtPiece/>, params);
 
       expect(await screen.findByText(mockPiece.artistInfo)).toBeInTheDocument();
-    });
-
-    it('should get the correct piece', async () => {
-      renderWithRouter(<ArtPiece/>, params);
-
-      await waitFor(() => expect(data.artGallery.getArt).toHaveBeenCalledWith({id: mockPiece.id, source: Source.AIC}));
     });
 
     it('should not have the error image', async () => {
@@ -84,7 +64,7 @@ describe('viewing a piece', () => {
   });
 
   test('when getting the piece has errored', async () => {
-    data.artGallery.getArt = () => failure(explanation(HTTPError.UNKNOWN as HTTPError));
+    fetchMock.mockResponse(HTTPError.SERVER_ERROR, {status: 400});
     renderWithRouter(<ArtPiece/>, {
       initialRoute: `${Paths.artGallery}/1234`,
       path: `${Paths.artGalleryPiece}`
