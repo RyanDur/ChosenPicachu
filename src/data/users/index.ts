74355fb81227249271c81d7f048b66b92a351713
Ryan Durling <ryan.p.durling@gmail.com>
2024-08-12 12:12:58 -0500

    move user helpers into users

--- src/data/users/index.ts
+++ src/data/users/index.ts
@@ -1,11 +1,11 @@
 import {asyncFailure, asyncSuccess, maybe, Result} from '@ryandur/sand';
 import {HTTPError} from '../types';
-import {User} from '../../components/UserInfo/types';
+import {AddressInfo, User, UserInfo} from '../../components/UserInfo/types';
 import {nanoid} from 'nanoid';
 import {createRandomUsers} from '../../__tests__/util/dummyData.tsx';
-
-const success = asyncSuccess;
-const failure = asyncFailure;
+import {AvatarGenerator} from 'random-avatar-generator';
+import {faker} from '@faker-js/faker';
+import {startOfDay} from 'date-fns';
 
 export interface UsersAPI {
   getAll: () => Result.Async<User[], HTTPError>;
@@ -16,13 +16,13 @@ export interface UsersAPI {
 }
 
 export const users = (randomUsers: User[] = createRandomUsers()): UsersAPI => ({
-  getAll: () => success(randomUsers),
+  getAll: () => asyncSuccess(randomUsers),
   get: id => maybe(randomUsers.find(user => user.id === id))
-    .map(user => success<User, HTTPError>(user))
-    .orElse(failure<User, HTTPError>(HTTPError.UNKNOWN)),
+    .map(user => asyncSuccess<User, HTTPError>(user))
+    .orElse(asyncFailure<User, HTTPError>(HTTPError.UNKNOWN)),
   add: user => {
     randomUsers = [{...user, id: nanoid()}, ...randomUsers];
-    return success(randomUsers);
+    return asyncSuccess(randomUsers);
   },
   update: user => {
     const friends: User[] = [user, ...user.friends.map(friend => ({
@@ -37,13 +37,54 @@ export const users = (randomUsers: User[] = createRandomUsers()): UsersAPI => ({
       friends.find(friend => friend.id === randomUser.id)
     ).orElse(randomUser));
 
-    return success(randomUsers);
+    return asyncSuccess(randomUsers);
   },
   delete: user => {
     randomUsers = randomUsers.map(randomUser => ({
       ...randomUser,
       friends: randomUser.friends.filter(friend => friend.id !== user.id)
     })).filter(randomUser => randomUser.id !== user.id);
-    return success(randomUsers);
+    return asyncSuccess(randomUsers);
   }
 });
+
+const generator = new AvatarGenerator();
+
+export const createUserInfo = (): UserInfo => ({
+  firstName: faker.person.firstName(),
+  lastName: faker.person.lastName(),
+  email: faker.internet.email(),
+  dob: startOfDay(faker.date.birthdate())
+});
+
+export const createAddress = (): AddressInfo => ({
+  city: faker.location.city(),
+  state: faker.location.state({ abbreviated: true }),
+  streetAddress: faker.location.street(),
+  streetAddressTwo: faker.location.secondaryAddress(),
+  zip: faker.location.zipCode()
+});
+
+const createDetails = (num = 10) => faker.lorem.sentences(randomNumberFromRange(2, num));
+
+export const createUser = (
+  worksFromHome = false,
+  address: () => AddressInfo = createAddress,
+  info: () => UserInfo = createUserInfo
+): User => {
+  const homeAddress = address();
+  return ({
+    id: nanoid(),
+    info: info(),
+    friends: [],
+    homeAddress,
+    workAddress: worksFromHome ? homeAddress : address(),
+    details: createDetails(),
+    avatar: generator.generateRandomAvatar()
+  });
+};
+
+export const randomNumberFromRange = (min: number, max = 6) => Math.floor(Math.random() * max) + min;
+export const usersApi = users([
+  ...Array(randomNumberFromRange(3, 15))].map(() => createUser(Math.random() > 0.5)
+));
