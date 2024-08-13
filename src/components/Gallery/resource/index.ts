84e896e6adb6b76749d1eded0d7dbd795ebf98c4
Ryan Durling <ryan.p.durling@gmail.com>
2024-08-13 14:53:56 -0500

    remove duplication

--- src/components/Gallery/resource/index.ts
+++ src/components/Gallery/resource/index.ts
@@ -6,31 +6,22 @@ import {aic} from './aic';
 import {harvard} from './harvard';
 import {rijks} from './rijks';
 
+const sources = {
+  [Source.AIC]: aic,
+  [Source.HARVARD]: harvard,
+  [Source.RIJKS]: rijks
+};
+
 export const art = {
   getAll: ({source, ...request}: GetAllArt): Result.Async<AllArt, HTTPError> =>
-    maybe({
-      [Source.AIC]: () => aic.allArt(request),
-      [Source.HARVARD]: () => harvard.allArt(request),
-      [Source.RIJKS]: () => rijks.allArt(request)
-    }[source])
-      .map(allArt => allArt())
+    maybe(sources[source]).map(api => api.allArt(request))
       .orElse(asyncFailure(HTTPError.UNKNOWN_SOURCE)),
 
   get: ({source, id}: GetArt): Result.Async<Art, HTTPError> =>
-    maybe({
-      [Source.AIC]: () => aic.art(id),
-      [Source.HARVARD]: () => harvard.art(id),
-      [Source.RIJKS]: () => rijks.art(id)
-    }[source])
-      .map(art => art())
+    maybe(sources[source]).map(api => api.art(id))
       .orElse(asyncFailure(HTTPError.UNKNOWN_SOURCE)),
 
   search: ({source, search}: SearchArt): Result.Async<SearchOptions, HTTPError> =>
-    maybe({
-      [Source.AIC]: () => aic.searchOptions(search),
-      [Source.HARVARD]: () => harvard.searchOptions(search),
-      [Source.RIJKS]: () => rijks.searchOptions(search)
-    }[source])
-      .map(searchOptions => searchOptions())
+    maybe(sources[source]).map(api => api.searchOptions(search))
       .orElse(asyncFailure(HTTPError.UNKNOWN_SOURCE))
 };
