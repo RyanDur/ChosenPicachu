1029dd6ca70e77de55de91b964f59d51b292de2b
Ryan Durling <ryan.p.durling@gmail.com>
2024-08-12 21:09:46 -0500

    make the matchOn more explicit

--- src/components/Gallery/resource/index.ts
+++ src/components/Gallery/resource/index.ts
@@ -1,4 +1,4 @@
-import {asyncFailure, Maybe, maybe, Result} from '@ryandur/sand';
+import {asyncFailure, maybe, Result} from '@ryandur/sand';
 import {GetAllArt, GetArt, SearchArt, Source} from './types/resource';
 import {AllArt, Art, SearchOptions} from './types/response';
 import {HTTPError} from '../../../data/types';
@@ -6,28 +6,31 @@ import {aic} from './aic';
 import {harvard} from './harvard';
 import {rijks} from './rijks';
 
-const matchSource = <T>(source: Source, options: Record<Source, () => T>): Maybe<T> =>
-  maybe(options[source]).map(option => option());
-
 export const resource = {
   getAllArt: ({source, ...request}: GetAllArt): Result.Async<AllArt, HTTPError> =>
-    matchSource(source, {
+    maybe({
       [Source.AIC]: () => aic.allArt(request),
       [Source.HARVARD]: () => harvard.allArt(request),
       [Source.RIJKS]: () => rijks.allArt(request)
-    }).orElse(asyncFailure(HTTPError.UNKNOWN_SOURCE)),
+    }[source])
+      .map(allArt => allArt())
+      .orElse(asyncFailure(HTTPError.UNKNOWN_SOURCE)),
 
   getArt: ({source, id}: GetArt): Result.Async<Art, HTTPError> =>
-    matchSource(source, {
+    maybe({
       [Source.AIC]: () => aic.art(id),
       [Source.HARVARD]: () => harvard.art(id),
       [Source.RIJKS]: () => rijks.art(id)
-    }).orElse(asyncFailure(HTTPError.UNKNOWN_SOURCE)),
+    }[source])
+      .map(art => art())
+      .orElse(asyncFailure(HTTPError.UNKNOWN_SOURCE)),
 
   searchForArt: ({source, search}: SearchArt): Result.Async<SearchOptions, HTTPError> =>
-    matchSource(source, {
+    maybe({
       [Source.AIC]: () => aic.searchOptions(search),
       [Source.HARVARD]: () => harvard.searchOptions(search),
       [Source.RIJKS]: () => rijks.searchOptions(search)
-    }).orElse(asyncFailure(HTTPError.UNKNOWN_SOURCE))
+    }[source])
+      .map(searchOptions => searchOptions())
+      .orElse(asyncFailure(HTTPError.UNKNOWN_SOURCE))
 };
