cc62a4d05bc676d0a457e1ee90c175fffb7df3b1
Ryan Durling <ryan.p.durling@gmail.com>
2024-08-12 20:11:47 -0500

    put the exported things towards the top, so it is the first thing someone sees after the imports.

--- src/components/Gallery/resource/aic/index.ts
+++ src/components/Gallery/resource/aic/index.ts
@@ -1,4 +1,12 @@
-import {AICAllArt, AICAllArtSchema, AICArt, AICArtSchema, AICPieceData, AICSearch, AICSearchSchema} from './types';
+import {
+  AICAllArt,
+  AICAllArtSchema,
+  AICArt,
+  AICArtSchema,
+  AICPieceData,
+  AICSearch,
+  AICSearchSchema,
+} from './types';
 import {toQueryString} from '../../../../util/URL';
 import {has, maybe} from '@ryandur/sand';
 import {aicDomain, defaultRecordLimit, defaultSearchLimit} from '../../../../config';
@@ -6,36 +14,8 @@ import {PATH} from '../../../../data/types';
 import {AllArt, Art, SearchOptions} from '../types/response';
 import {validate} from '../../../../data/validate';
 import {http} from '../../../../data/http';
-import {GetAllArtRequest} from '../types/resource';
+import {GetAllArtRequest, Query} from '../types/resource';
 
-export const shapeOfAICResponse = ['id', 'title', 'image_id', 'artist_display', 'term_titles', 'thumbnail'];
-export const aicToPiece = (data: AICArt): Art => ({
-  id: String(data.id),
-  title: data.title,
-  image: `https://www.artic.edu/iiif/2/${data.image_id}/full/2000,/0/default.jpg`,
-  artistInfo: data.artist_display,
-  altText: data.thumbnail?.alt_text || data.term_titles.join(' ') || ''
-});
-
-interface Query {
-  path?: (string | number)[];
-  params?: Record<string, unknown>
-}
-
-const endpoint = ({path, params = {}}: Query): PATH => {
-  const {search, limit = defaultRecordLimit, page, ...rest} = params;
-  const queryString = toQueryString({
-    q: search, fields: shapeOfAICResponse,
-    page, limit, ...rest
-  });
-  return maybe(search).map(() => [[
-    aicDomain,
-    'search'
-  ].join('/'), queryString].join('')).orElse([[
-    aicDomain,
-    path?.filter(has).join('/')
-  ].filter(has).join('/'), queryString].join(''));
-};
 export const aic = {
   allArt: ({page, size, search}: GetAllArtRequest) => http
     .get(endpoint({params: {page, size, search}}))
@@ -65,4 +45,31 @@ export const aic = {
     .map(({data}: AICSearch): SearchOptions => data
       .map(({suggest_autocomplete_all}) => suggest_autocomplete_all[1])
       .flatMap(option => option.input))
+};
+
+const aicToPiece = (data: AICArt): Art => ({
+  id: String(data.id),
+  title: data.title,
+  image: `https://www.artic.edu/iiif/2/${data.image_id}/full/2000,/0/default.jpg`,
+  artistInfo: data.artist_display,
+  altText: data.thumbnail?.alt_text || data.term_titles.join(' ') || ''
+});
+
+const endpoint = ({path, params = {}}: Query): PATH => {
+  const {search, limit = defaultRecordLimit, page, ...rest} = params;
+  const queryString = toQueryString({
+    q: search,
+    fields: ['id', 'title', 'image_id', 'artist_display', 'term_titles', 'thumbnail'],
+    page,
+    limit,
+    ...rest
+  });
+
+  return maybe(search).map(() => [[
+    aicDomain,
+    'search'
+  ].join('/'), queryString].join('')).orElse([[
+    aicDomain,
+    path?.filter(has).join('/')
+  ].filter(has).join('/'), queryString].join(''));
 };
