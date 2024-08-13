7da81e8e13441338f38c67bbfb6604020589adb1
Ryan Durling <ryan.p.durling@gmail.com>
2024-08-12 21:03:41 -0500

    make the matchOn more explicit

--- src/components/Gallery/resource/types/resource.ts
+++ src/components/Gallery/resource/types/resource.ts
@@ -1,11 +1,8 @@
-import {matchOn} from '@ryandur/sand';
-
 export enum Source {
     AIC = 'aic',
     HARVARD = 'harvard',
     RIJKS = 'rijksstudio'
 }
-export const matchSource = matchOn(Object.values(Source));
 
 export interface SearchArt {
     search: string;
