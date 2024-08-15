4faab2536bb63ad01bb2ee531bf5995feff1975a
Ryan Durling <ryan.p.durling@gmail.com>
2024-08-14 20:25:04 -0500

    mov gallery context to gallery scope. rest of app doesn't need it.

--- src/routes/ArtGalleryPage.tsx
+++ src/routes/ArtGalleryPage.tsx
@@ -1,11 +1,15 @@
 import {FC} from 'react';
 import {Gallery, GalleryNav, PageControl, Search} from '../components';
 import {SideNav} from './BasePage/SideNav';
+import {GalleryContext, useGalleryContext} from '../components/Gallery/Context';
+import {AllArt} from '../components/Gallery/resource/types/response';
 import './BasePage.css';
 import './BasePage.layout.css';
 
-export const ArtGalleryPage: FC = () =>
-  <>
+export const ArtGalleryPage: FC<Partial<{
+  galleryState: AllArt
+}>> = ({galleryState}) =>
+  <GalleryContext.Provider value={useGalleryContext(galleryState)}>
     <header id="app-header" data-testid="header">
       <h1 className="title ellipsis">About</h1>
       <Search id="gallery-search"/>
@@ -22,5 +26,5 @@ export const ArtGalleryPage: FC = () =>
     <footer id={'app-footer'} className={'stick-to-bottom'} data-testid="footer">
       <GalleryNav id="gallery-nav"/>
     </footer>
-  </>;
+  </GalleryContext.Provider>;
 
