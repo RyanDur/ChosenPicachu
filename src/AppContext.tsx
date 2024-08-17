3e54c1a5c197836ae4c68d628409ab77bc3f4f8a
Ryan Durling <ryan.p.durling@gmail.com>
2024-08-17 18:37:34 -0500

    move gallery context back into the central context

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -1,11 +1,14 @@
 import {FC, PropsWithChildren} from 'react';
 import {ArtPieceContext, useArtPieceContext} from './components';
 import {AllArt, Art} from './components/Gallery/resource/types/response';
+import {GalleryContext} from './components/Gallery/Context';
 
 export const AppContext: FC<PropsWithChildren & Partial<{
   galleryState: AllArt,
   pieceState: Partial<Art>
 }>> = ({children, pieceState}) =>
-  <ArtPieceContext.Provider value={useArtPieceContext(pieceState)}>
-    {children}
-  </ArtPieceContext.Provider>;
+  <GalleryContext>
+    <ArtPieceContext.Provider value={useArtPieceContext(pieceState)}>
+      {children}
+    </ArtPieceContext.Provider>
+  </GalleryContext>;
