4138b309233e04adf1ae27ad5fe7ded8b11377ab
Ryan Durling <ryan.p.durling@gmail.com>
2024-08-17 18:13:41 -0500

    remove unused imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,4 @@
 export {Home} from './Home';
 export {About} from './About';
 export {Users} from './Users';
-export {Gallery, GalleryNav, GalleryTitle, PageControl} from './Gallery';
 export {ArtPiece, ArtPieceContext, useArtPieceContext} from './Gallery/ArtPiece';
-export {Search} from './Gallery/Search';
