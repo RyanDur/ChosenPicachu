45b06910a72cc04522a253995f7363e55232b4fa
Ryan Durling <ryan.p.durling@gmail.com>
2024-08-18 13:14:19 -0500

    move gallery routes to gallery

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,7 +1,25 @@
 import {ArtGalleryPage} from './ArtGalleryPage';
 import {ArtGalleryPiecePage} from './ArtGalleryPiecePage';
+import {Paths} from '../../routes/Paths';
+import {Outlet} from 'react-router-dom';
+import {GalleryPaths} from './GalleryPaths';
 
-export {
-  ArtGalleryPage,
-  ArtGalleryPiecePage
+const GalleryRouter = () => (
+  <Outlet/>
+);
+
+export const Gallery = {
+  path: `${Paths.artGallery}/*`,
+  element: <GalleryRouter/>,
+  children: [
+    {
+      path: GalleryPaths.home,
+      element: <ArtGalleryPage/>
+    },
+    {
+      path: GalleryPaths.piece,
+      element: <ArtGalleryPiecePage/>
+    }
+  ]
 };
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import {AboutPage, HomePage, UsersPage} from './routes';
 import {AppContext} from './AppContext';
-import * as ReactDom  from "react-dom/client";
+import * as ReactDom from "react-dom/client";
 import {StrictMode} from "react";
 import {Paths} from './routes/Paths';
+import {Gallery} from './components/Gallery';
 import './index.css';
-import {ArtGalleryPage, ArtGalleryPiecePage} from './components/Gallery';
 
 const router = createBrowserRouter([
   {
@@ -20,14 +20,7 @@ const router = createBrowserRouter([
     path: Paths.users,
     element: <UsersPage/>
   },
-  {
-    path: Paths.artGallery,
-    element: <ArtGalleryPage/>
-  },
-  {
-    path: Paths.artGalleryPiece,
-    element: <ArtGalleryPiecePage/>
-  }
+  Gallery
 ]);
 
 ReactDom.createRoot(document.getElementById('root')!).render(
