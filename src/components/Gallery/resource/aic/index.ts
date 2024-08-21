4bcdb71c114310a17516a1dbc09d2d5b609b4d0f
Ryan Durling <ryan.p.durling@gmail.com>
2024-08-21 16:27:47 -0500

    fix search

--- src/components/Gallery/resource/aic/index.ts
+++ src/components/Gallery/resource/aic/index.ts
@@ -1,56 +1,73 @@
 import {
-  AICAllArtResponse,
-  AICAllArtSchema,
-  AICArt,
-  AICArtSchema,
-  AICPieceData,
-  AICSearchResponse,
-  AICSearchSchema
+    AICAllArtResponse,
+    AICAllArtSchema,
+    AICArt,
+    AICArtSchema,
+    AICPieceData,
+    AICSearchResponse,
+    AICSearchSchema
 } from './types';
 import {toQueryString} from '../../../../util/URL';
 import {aicDomain, defaultRecordLimit, defaultSearchLimit} from '../../../../config';
 import {AllArt, Art, SearchOptions} from '../types/response';
 import {validate} from '../../../../data/validate';
 import {http} from '../../../../data/http';
-import {GetAllArtRequest} from '../types/resource';
+import {GetAllArtRequest, Query} from '../types/resource';
+import {PATH} from "../../../../data/types";
+import {has, maybe} from "@ryandur/sand";
 
 export const fields = ['id', 'title', 'image_id', 'artist_display', 'term_titles', 'thumbnail'];
 
 export const aic = {
-  allArt: ({page, search, size = defaultRecordLimit}: GetAllArtRequest) => http
-    .get(`${aicDomain}${toQueryString({q: search, fields, page, limit: size})}`)
-    .mBind(validate(AICAllArtSchema))
-    .map(({pagination, data}: AICAllArtResponse): AllArt => ({
-      pagination: {
-        total: pagination.total,
-        limit: pagination.limit,
-        totalPages: pagination.total_pages,
-        currentPage: pagination.current_page
-      },
-      pieces: data.map(aicToPiece)
-    })),
+    allArt: ({page, search, size = defaultRecordLimit}: GetAllArtRequest) => http
+        .get(endpoint({params: {page, search, size}}))
+        .mBind(validate(AICAllArtSchema))
+        .map(({pagination, data}: AICAllArtResponse): AllArt => ({
+            pagination: {
+                total: pagination.total,
+                limit: pagination.limit,
+                totalPages: pagination.total_pages,
+                currentPage: pagination.current_page
+            },
+            pieces: data.map(aicToPiece)
+        })),
 
-  art: (id: string) => http
-    .get(`${aicDomain}/${id}${toQueryString({fields})}`)
-    .mBind(validate(AICArtSchema))
-    .map(({data}: AICPieceData): Art => aicToPiece(data)),
+    art: (id: string) => http
+        .get(`${aicDomain}/${id}${toQueryString({fields})}`)
+        .mBind(validate(AICArtSchema))
+        .map(({data}: AICPieceData): Art => aicToPiece(data)),
 
-  searchOptions: (search: string) => http
-    .get(`${aicDomain}/search${toQueryString({
-      'query[term][title]': search,
-      fields: 'suggest_autocomplete_all',
-      limit: defaultSearchLimit
-    })}`)
-    .mBind(validate(AICSearchSchema))
-    .map(({data}: AICSearchResponse): SearchOptions => data
-      .map(({suggest_autocomplete_all}) => suggest_autocomplete_all[1])
-      .flatMap(option => option.input))
+    searchOptions: (search: string) => http
+        .get(`${aicDomain}/search${toQueryString({
+            'query[term][title]': search,
+            fields: 'suggest_autocomplete_all',
+            limit: defaultSearchLimit
+        })}`)
+        .mBind(validate(AICSearchSchema))
+        .map(({data}: AICSearchResponse): SearchOptions => data
+            .map(({suggest_autocomplete_all}) => suggest_autocomplete_all[1])
+            .flatMap(option => option.input))
 };
 
 const aicToPiece = (data: AICArt): Art => ({
-  id: String(data.id),
-  title: data.title,
-  image: `https://www.artic.edu/iiif/2/${data.image_id}/full/2000,/0/default.jpg`,
-  artistInfo: data.artist_display,
-  altText: data.thumbnail?.alt_text || data.term_titles.join(' ') || ''
+    id: String(data.id),
+    title: data.title,
+    image: `https://www.artic.edu/iiif/2/${data.image_id}/full/2000,/0/default.jpg`,
+    artistInfo: data.artist_display,
+    altText: data.thumbnail?.alt_text || data.term_titles.join(' ') || ''
 });
+
+const endpoint = ({path, params = {}}: Query): PATH => {
+    const {search, limit = defaultRecordLimit, page} = params;
+    const queryString = toQueryString({
+        q: search, fields, page, limit
+    });
+
+    return maybe(search).map(() => [[
+        aicDomain,
+        'search'
+    ].join('/'), queryString].join('')).orElse([[
+        aicDomain,
+        path?.filter(has).join('/')
+    ].filter(has).join('/'), queryString].join(''));
+};
