4dfc8b6bffc62070ab89123eee4445343ce296da
Ryan Durling <ryan.p.durling@gmail.com>
2024-10-01 19:33:41 -0500

    update style the about page title

--- src/components/About/ZIndexDemo/NaturalZIndex.tsx
+++ src/components/About/ZIndexDemo/NaturalZIndex.tsx
@@ -1,18 +1,19 @@
 import {FC, useState} from "react";
-import {not} from "@ryandur/sand";
+import {not as toggle} from "@ryandur/sand";
 import {PropsWithClassName} from "../../_types";
+import {classNames} from "../../../util/classNames";
 import './styles.css';
 
 export const NaturalZIndex: FC<PropsWithClassName> = ({className}) => {
-    const [isRelative, updatePosition] = useState(true);
-    const onClick = () => updatePosition(not(isRelative));
+  const [isCollapsed, updateCollapsed] = useState(true);
+  const onClick = () => updateCollapsed(toggle(isCollapsed));
 
-    return <article id="z-index-demo" className={className}>
-        <button className='primary' onClick={onClick}>{isRelative ? 'Collapse' : 'Expand'}</button>
-        <section className='demo-container'>
-            <p className={`card positioned-${isRelative ? 'relative' : 'absolute'}`}>First</p>
-            <p className={`card positioned-${isRelative ? 'relative' : 'absolute'}`}>Second</p>
-            <p className={`card positioned-${isRelative ? 'relative' : 'absolute'}`}>Third</p>
-        </section>
-    </article>;
+  return <article id="z-index-demo" className={className}>
+    <button className='primary' onClick={onClick}>{isCollapsed ? 'Collapse' : 'Expand'}</button>
+    <section className='demo-container'>
+      <p className={classNames(`card`, 'first', isCollapsed && 'closed')}>First</p>
+      <p className={classNames(`card`, 'second', isCollapsed && 'closed')}>Second</p>
+      <p className={classNames(`card`, 'third', isCollapsed && 'closed')}>Third</p>
+    </section>
+  </article>;
 };
