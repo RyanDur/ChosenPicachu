12e2967702119e4de3e0895861a3706a3e1124db
Ryan Durling <ryan.p.durling@gmail.com>
2024-10-20 18:22:28 -0500

    Revert "no need to do higher order components."
    This reverts commit cb676b7a7f171c9ec5d283e27a0190388c9acf7a.

--- src/components/About/DragAndDrop/HideElemOnDragSortableList.tsx
+++ src/components/About/DragAndDrop/HideElemOnDragSortableList.tsx
@@ -1,5 +1,5 @@
-import {FC, useState} from 'react';
-import {Draggable} from './Draggable';
+import {cloneElement, DragEvent, FC, ReactElement, useState} from 'react';
+import {Draggable, DraggableListItemProps} from './Draggable';
 import {array} from '../../../helpers';
 import {classNames} from '../../../util/classNames';
 import './styles.css';
@@ -9,31 +9,43 @@ export const HideElemOnDragSortableList: FC<{ list: Set<string> }> = ({list}) =>
   const [currentList, updateList] = useState<string[]>([...list]);
   const [dragOverIndex, updateIndex] = useState<number>(-1);
   const [draggedItem, updateDraggedItem] = useState<string>();
-  const [hide, updateHide] = useState(false);
 
   return <ul onDrop={event => {
     event.currentTarget.blur();
   }} onDragLeave={() => updateIndex(-1)} className='sortable-list'>{
     currentList.map((item, index) =>
       <li className='item' key={item}>
-        <Draggable
-          className={classNames('visible', hide && 'invisible')}
-          onDragEnd={() => {
-            updateHide(false);
-            if (draggedItem && dragOverIndex >= 0) {
-              updateList((oldList) => array
-                .moveToIndex(dragOverIndex, draggedItem, oldList));
-            }
-          }}
-          onDragStart={() => {
-            updateHide(true);
-            updateDraggedItem(item);
-          }}
-          onDragOver={() => {
-            updateIndex(index);
-          }}
-          label={item}
-        >{item}</Draggable>
+        <HideOnDrag>
+          <Draggable
+            onDragEnd={() => {
+              if (draggedItem && dragOverIndex >= 0) {
+                updateList((oldList) => array
+                  .moveToIndex(dragOverIndex, draggedItem, oldList));
+              }
+            }}
+            onDragStart={() => updateDraggedItem(item)}
+            onDragOver={() => updateIndex(index)}
+            label={item}
+          >{item}</Draggable>
+        </HideOnDrag>
       </li>
     )}</ul>;
+};
+
+export const HideOnDrag: FC<{ children: ReactElement<DraggableListItemProps> }> = ({children}) => {
+  const [hide, updateHide] = useState(false);
+  const {className, onDragStart, onDragEnd, ...props} = children.props;
+
+  return cloneElement(children, {
+    ...props,
+    className: classNames('visible', hide && 'invisible', className),
+    onDragStart: (event: DragEvent<HTMLElement>) => {
+      updateHide(true);
+      onDragStart(event);
+    },
+    onDragEnd: (event: DragEvent<HTMLElement>) => {
+      updateHide(false);
+      onDragEnd(event);
+    }
+  });
 };
