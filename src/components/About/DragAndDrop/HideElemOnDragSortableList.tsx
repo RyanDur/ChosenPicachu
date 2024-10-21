e8c3e59505adb1956f2fcea3274dc4981dad3c3f
Ryan Durling <ryan.p.durling@gmail.com>
2024-10-20 20:55:30 -0500

    compose differently

--- src/components/About/DragAndDrop/HideElemOnDragSortableList.tsx
+++ src/components/About/DragAndDrop/HideElemOnDragSortableList.tsx
@@ -1,4 +1,4 @@
-import {cloneElement, DragEvent, FC, ReactElement, useState} from 'react';
+import {FC, useState} from 'react';
 import {Draggable, DraggableListItemProps} from './Draggable';
 import {array} from '../../../helpers';
 import {classNames} from '../../../util/classNames';
@@ -10,42 +10,45 @@ export const HideElemOnDragSortableList: FC<{ list: Set<string> }> = ({list}) =>
   const [dragOverIndex, updateIndex] = useState<number>(-1);
   const [draggedItem, updateDraggedItem] = useState<string>();
 
-  return <ul onDrop={event => {
-    event.currentTarget.blur();
-  }} onDragLeave={() => updateIndex(-1)} className='sortable-list'>{
+  return <ul onDragLeave={() => updateIndex(-1)} className='sortable-list'>{
     currentList.map((item, index) =>
       <li className='item' key={item}>
-        <HideOnDrag>
-          <Draggable
-            onDragEnd={() => {
-              if (draggedItem && dragOverIndex >= 0) {
-                updateList((oldList) => array
-                  .moveToIndex(dragOverIndex, draggedItem, oldList));
-              }
-            }}
-            onDragStart={() => updateDraggedItem(item)}
-            onDragOver={() => updateIndex(index)}
-            label={item}
-          >{item}</Draggable>
-        </HideOnDrag>
+        <HideOnDrag
+          onDragEnd={() => {
+            if (draggedItem && dragOverIndex >= 0) {
+              updateList((oldList) => array
+                .moveToIndex(dragOverIndex, draggedItem, oldList));
+            }
+          }}
+          onDragStart={() => updateDraggedItem(item)}
+          onDragOver={() => updateIndex(index)}
+          label={item}
+        >{item}</HideOnDrag>
       </li>
     )}</ul>;
 };
 
-export const HideOnDrag: FC<{ children: ReactElement<DraggableListItemProps> }> = ({children}) => {
+const HideOnDrag: FC<DraggableListItemProps> = ({
+  children,
+  className,
+  label,
+  onDragStart,
+  onDragEnd,
+  onDragOver
+}) => {
   const [hide, updateHide] = useState<'hide'>();
-  const {className, onDragStart, onDragEnd, ...props} = children.props;
 
-  return cloneElement(children, {
-    ...props,
-    className: classNames(hide, className),
-    onDragStart: (event: DragEvent<HTMLElement>) => {
-      onDragStart(event);
-      updateHide('hide');
-    },
-    onDragEnd: (event: DragEvent<HTMLElement>) => {
+  return <Draggable
+    className={classNames(hide, className)}
+    onDragEnd={(event) => {
       onDragEnd(event);
       updateHide(undefined);
-    }
-  });
+    }}
+    onDragStart={(event) => {
+      onDragStart(event);
+      updateHide('hide');
+    }}
+    onDragOver={(event) => onDragOver(event)}
+    label={label}
+  >{children}</Draggable>;
 };
